0469b66b832c74bcdfa8441e730ddb3a360d6626
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2023-08-20 09:39:29 -0300

    Setup fake home directory for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-require "pod"
+require 'pod'
+require 'fileutils'
+
+TMP_DIR = (Dir.pwd + '/tmp').freeze
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -11,5 +14,20 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.around :each do |test|
+    if !Dir.exist?(TMP_DIR)
+      FileUtils.mkdir_p(TMP_DIR)
+    end
+
+    ENV['HOME'] = TMP_DIR
+
+    test.run
+
+    config_dir = TMP_DIR + '/.config'
+    if Dir.exist?(config_dir)
+      FileUtils.rm_r(config_dir)
+    end
   end
 end

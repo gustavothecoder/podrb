bb5e60b357a5eaf02ff8693c74181ec6cc4411db
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2023-09-14 09:38:39 -0300

    Test if `Pod::Storage::SQL#execute` works with DML statements

--- spec/pod/storage/sql_spec.rb
+++ spec/pod/storage/sql_spec.rb
@@ -40,5 +40,62 @@ RSpec.describe Pod::Storage::SQL do
         expect { db.execute(statement) }.to raise_error(Pod::Storage::Exceptions::WrongSyntax)
       end
     end
+
+    context "when a DML statement is passed" do
+      it "executes the statement and returns true" do
+        FileUtils.mkdir_p(TestHelpers::Path.config_dir)
+        db = described_class.new(db: TestHelpers::Path.db_dir)
+        db.execute <<-SQL
+          create table podcasts (
+            id int primary key,
+            name text not null,
+            description text,
+            feed text not null,
+            website text
+          );
+        SQL
+
+        result = db.execute <<-SQL
+          insert into podcasts
+          (name, description, feed, website)
+          values (
+            "Podcast",
+            "A really good podcast.",
+            "https://www.podcast.com/feed.xml",
+            "https://www.podcast.com"
+          );
+        SQL
+
+        expect(result).to eq(true)
+      end
+    end
+
+    context "when an invalid DML statement is passed" do
+      it "doesn't execute the statement and raises an exception" do
+        FileUtils.mkdir_p(TestHelpers::Path.config_dir)
+        db = described_class.new(db: TestHelpers::Path.db_dir)
+        db.execute <<-SQL
+          create table podcasts (
+            id int primary key,
+            name text not null,
+            description text,
+            feed text not null,
+            website text
+          );
+        SQL
+        invalid_dml_statement = <<-SQL
+          insert into podcasts
+          (invalid, description, feed, website)
+          values (
+            "Podcast",
+            "A really good podcast.",
+            "https://www.podcast.com/feed.xml",
+            "https://www.podcast.com"
+          );
+        SQL
+
+        expect{ db.execute(invalid_dml_statement) }.to raise_error(Pod::Storage::Exceptions::WrongSyntax)
+      end
+    end
   end
 end
